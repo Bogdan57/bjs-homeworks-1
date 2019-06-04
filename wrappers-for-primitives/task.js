6e80367d33d6acf478d371e3eadd39076538fafc
Bogdan Novikov <fikis57@mail.ru>
2019-06-04 15:40:34 +0300

    add wrappers

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -13,6 +13,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
     //return totalAmount;
+    function checkDataType(item) {
+        if ( typeof item === 'number' ) {
+            item = item;
+        } else if ( typeof item === 'string' ) {
+            item = Number(item);
+            if ( typeof item === 'string' || isNaN( Number(item) ) ) {
+            	console.log(`Параметр содержит неправильное значение ${item}`);
+            }
+        } else {
+            console.log(`Параметр содержит неправильное значение ${item}`);
+        }
+
+        return item;
 }
 
 function sayHello() {
@@ -25,4 +38,14 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+    let greeting;
+
+    if ( typeof name === 'undefined' || name === null || name === "" ) {
+        greeting = 'Привет, мир! Меня зовут Аноним.';
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    }
+
+    return greeting;
 }
+console.log(getGreeting());
